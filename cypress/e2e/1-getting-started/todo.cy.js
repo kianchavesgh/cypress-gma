af47c7edee5fab7136867a0eee441d78940f5c09
Kian Chaves <kianchaves@live.com>
2025-08-07 19:42:48 -0300

    removed e2e 2

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -23,7 +23,7 @@ describe('example to-do app', () => {
   it('displays two todo items by default', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
+    // which are the two default items
     cy.get('.todo-list li').should('have.length', 2)
 
     // We can go even further and check that the default todos each contain
@@ -54,7 +54,7 @@ describe('example to-do app', () => {
     cy.get('.todo-list li')
       .should('have.length', 3)
       .last()
-      .should('have.text', newItem)
+      .should('have.text', 'Feed the cat');
   })
 
   it('can check off an item as completed', () => {
